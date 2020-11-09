774d43a0d4a5d01ab5efdc27a48f8ff1498ca7d9
Pawel Dera <236517@edu.p.lodz.pl>
2020-11-09 13:02:08 +0100

    Added shooting mechanics and bullet prefab

---
+++ Assets/Scripts/Player/PlayerShoot.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerShoot : MonoBehaviour
+{
+    public Transform firePoint;
+    public GameObject bullet;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Instantiate(bullet, firePoint.position, firePoint.rotation);
+        }
+    }
+}
---
+++ Assets/Scripts/Weapons/BulletMechanics.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BulletMechanics : MonoBehaviour
+{
+    private Rigidbody thisRb;
+    private Vector3 bulletForce;
+    private float force = 1000f;
+
+    void Awake()
+    {
+        thisRb = gameObject.GetComponent<Rigidbody>();
+        bulletForce = new Vector3(0f, 0f, force);
+    }
+
+    void Start()
+    {
+        thisRb.AddForce(bulletForce);
+    }
+
+}
