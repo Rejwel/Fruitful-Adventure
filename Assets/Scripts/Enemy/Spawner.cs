68ca3378e216b1ae89275c555bb6ec38bcc74a9f
Pawel Dera <236517@edu.p.lodz.pl>
2020-12-01 19:39:22 +0100

    Adding spawner

---
+++ Assets/Scripts/Enemy/Spawner.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    // SCHEME
+    //    MAGE MELEE RANGE TANK
+    //    POSITIONS [][]
+    //    I        I    II    III    IV      MELEE TANK
+    //    II       I    II    III    IV      MELEE TANK
+    //    III      I    II    III    IV      RANGED MAGE
+    //    VI       I    II    III    IV      RANGED MAGE
+    //    
+    //
+
+    private Transform [][] spawnPoints;
+    private Transform spawnPoint;
+    private void Start()
+    {
+        spawnPoint = this.transform;
+        setPositions(spawnPoints);
+    }
+
+    void setPositions(Transform [][] spawnPoints)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                string obj = "spawn" + i + j;
+                //print(obj);
+                //print(GameObject.Find(obj).transform);
+                //spawnPoints[i][j] = GameObject.Find(obj).transform;
+                //print(spawnPoints[i][j]);
+            }
+        }
+    }
+    
+    
+}
