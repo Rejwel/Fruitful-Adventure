938d3ec72b7eeef7c11c793a79ad0c6c2d1d33b7
Pawel Dera <236517@edu.p.lodz.pl>
2020-12-04 17:24:51 +0100

    Wave manager update

---
+++ Assets/Scripts/Wave/Spawner.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mime;
+using UnityEditorInternal;
+using UnityEngine;
+using Random = UnityEngine.Random;
+using UnityEngine.UI;
+
+public class Spawner : MonoBehaviour
+{
+    // SCHEME
+    //    MAGE MELEE RANGE TANK
+    //    POSITIONS [][]
+    //    I        I    II    III    IV      MELEE TANK
+    //    II       I    II    III    IV      MELEE TANK
+    //    III      I    II    III    IV      RANGED MAGE
+    //    VI       I    II    III    IV      RANGED MAGE
+    //    
+    //
+    // SETTING SPAWNPOINTS
+    private int enemyCounter;
+    
+    private Transform [,] spawnPoints = new Transform[4, 4];
+    private Transform [] spawnPointsArray;
+    List<Transform> spawnPointsList = new List<Transform>();
+    public GameObject [] enemies;
+    private void Start()
+    {
+        setPositions(spawnPoints);
+
+        spawnPointsArray = GetComponentsInChildren<Transform>();
+        spawnPointsList = spawnPointsArray.ToList();
+        spawnPointsList.Remove(spawnPointsList[0]);
+    }
+    void setPositions(Transform [,] spawnPoints)
+    {
+        for (int i = 0; i < 4; i++)
+        {
+            for (int j = 0; j < 4; j++)
+            {
+                string obj = "spawn" + i + j;
+                spawnPoints[i, j] = GameObject.Find(obj).transform;
+            }
+        }
+    }
+
+    public void spawnEnemies(List<Transform> Spawnpoints, GameObject [] enemies)
+    {
+        foreach (var spawnpoint in Spawnpoints)
+        {
+            //Instantiate(enemies[Random.Range(0,4)], spawnpoint.position, spawnpoint.rotation);
+            Instantiate(enemies[1], spawnpoint.position, spawnpoint.rotation);
+            enemyCounter++;
+        }
+    }
+
+    public void EnemyKill()
+    {
+        enemyCounter--;
+    }
+
+    public int getEmemies()
+    {
+        return enemyCounter;
+    }
+
+    public List<Transform> getSpawnpoints()
+    {
+        return spawnPointsList;
+    }
+
+}
---
+++ Assets/Scripts/Wave/WaveManager.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class WaveManager : MonoBehaviour
+{
+    private int waveCounter;
+    private float prepareTime = 5f;
+    private float waveTime = 5f;
+    
+    private bool isPreparation = true;
+    private bool spawning = false;
+    private bool lastSpawn = true;
+    
+    private Text timer;
+    private Text waveCount;
+    private Text enemiesLeft;
+
+    private Spawner spawner;
+    
+    public GameObject [] enemies;
+    
+    void Start()
+    {
+        spawner = GameObject.FindObjectOfType<Spawner>();
+        timer = GameObject.Find("TimerManager").GetComponent<Text>();
+        waveCount = GameObject.Find("WaveManager").GetComponent<Text>();
+        enemiesLeft = GameObject.Find("EnemiesRemain").GetComponent<Text>();
+        
+        waveCounter = 1;
+    }
+
+    private void Update()
+    {
+        
+            enemiesLeft.text = spawner.getEmemies().ToString();
+    
+                if (waveCounter == 3 && spawner.getEmemies() == 0)
+                {
+                    waveCount.text = "you Won!";
+                    timer.text = "";
+                }
+                else if (waveCounter == 3 && spawner.getEmemies() > 0)
+                {
+                    waveCount.text = "Last wave, Kill all the enemies!";
+                    timer.text = "";
+                }
+                else
+                {
+                    if (spawning && isPreparation == false)
+                    {
+                        spawner.spawnEnemies(spawner.getSpawnpoints(), enemies);
+                        spawning = false;
+                    }
+    
+                    if (waveTime <= 0f && isPreparation == false)
+                    {
+                        isPreparation = true;
+                        waveCounter++;
+                    }
+                    if(waveCounter == 3) spawner.spawnEnemies(spawner.getSpawnpoints(), enemies);
+                    else
+                    {
+                        if (prepareTime > 0f && isPreparation) 
+                        {
+                            waveTime = 5f;
+                            waveCount.text = $"Prepare for the {waveCounter} Wave";
+                            prepareTime -= Time.deltaTime;
+                            timer.text = prepareTime.ToString("f2");
+                            spawning = true;
+                        }
+                        else
+                        {
+                            isPreparation = false;
+                            prepareTime = 5f;
+                            waveCount.text = waveCounter + " Wave";
+                            timer.text = waveTime.ToString("f2"); 
+                            waveTime -= Time.deltaTime;
+                        } 
+                    }
+                }
+        }
+}
