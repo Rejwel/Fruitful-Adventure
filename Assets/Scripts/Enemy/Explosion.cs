835bee7427689e04680de1e0cfc78c750cc3a7bb
Mateusz Banasik <kilifugame@gmail.com>
2020-12-08 22:02:33 +0100

    tylko skrypt explosion

---
+++ Assets/Scripts/Enemy/Explosion.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float cubeSize = 0.2f;
+    public int cubesInRow = 5;
+
+    float cubesPivotDistance;
+    Vector3 cubesPivot;
+    public float explosionRadius = 4f;
+    public float explosionForce = 100f;
+    public float explosionUpward = 0.2f;
+
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        cubesPivotDistance = cubeSize * cubesInRow / 2;
+        cubesPivot = new Vector3(cubesPivotDistance, cubesPivotDistance, cubesPivotDistance);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.name == "Ground")
+        {
+            explode();
+        }
+    }
+
+    public void explode()
+    {
+        gameObject.SetActive(false);
+
+        for (int x = 0; x < cubesInRow; x++)
+        {
+            for (int y = 0; y < cubesInRow; y++)
+            {
+                for (int z = 0; z < cubesInRow; z++)
+                {
+                    createPiece(x, y, z);
+                }
+            }
+        }
+
+        Vector3 explosionPos = transform.position;
+        Collider[] colliders = Physics.OverlapSphere(explosionPos, explosionRadius);
+        foreach (Collider hit in colliders)
+        {
+            Rigidbody rb = hit.GetComponent<Rigidbody>();
+            if (rb != null)
+            {
+                rb.AddExplosionForce(explosionForce, transform.position, explosionRadius, explosionUpward);
+            }
+        }
+
+    }
+
+    public void createPiece(int x, int y, int z)
+    {
+        GameObject piece;
+        piece = GameObject.CreatePrimitive(PrimitiveType.Cube);
+
+        piece.transform.position = transform.position + new Vector3(cubeSize * x, cubeSize * y, cubeSize * z) - cubesPivot;
+        piece.transform.localScale = new Vector3(cubeSize, cubeSize, cubeSize);
+        piece.AddComponent<Collider>().isTrigger = true;
+        piece.AddComponent<Rigidbody>();
+        piece.AddComponent<PlayerMovement>();
+        piece.gameObject.tag = "Money";
+
+
+        piece.GetComponent<Rigidbody>().mass = cubeSize;
+
+
+    }
+
+
+
+
+}
