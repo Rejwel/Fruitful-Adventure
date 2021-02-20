af297db6d9dd85160d655069e177110b5f388570
Mateusz Banasik <kilifugame@gmail.com>
2021-02-20 14:25:12 +0100

    Udoskonalenie strzeleckich skryptów

---
+++ Assets/Scripts/Enemy/EnemyCamera.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyCamera : MonoBehaviour
+{
+    private Transform gracz;
+    protected GameObject graczObiekt;
+    protected Vector3 hitPoint;
+    public float predkoscObrotu = 6.0f;
+    public bool gladkiObrot = true;
+    public float predkoscRuchu = 5.0f;
+    private Transform mojObiekt;
+    private FollowingAndShooting ShootScript;
+    private Vector3 pozycjaGraczaXYZ;
+
+    void Start()
+    {
+        graczObiekt = GameObject.FindWithTag("Player");
+        gracz = graczObiekt.transform;
+
+        mojObiekt = transform;
+        
+    }
+
+   
+
+   
+
+   
+}
