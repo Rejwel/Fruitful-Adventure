ae33826cbd1ea5d68771490d68b71114f797b6e9
Paul Dera <paweldera01@gmail.com>
2021-03-18 18:31:23 +0100

    Branch test

--- Assets/Scripts/Enemy/EnemyRanged.cs
+++ Assets/Scripts/Enemy/EnemyRanged.cs
@@ -44,7 +44,6 @@ public class EnemyRanged : MonoBehaviour
 
     void Start()
     {
-        //Following
         StartCoroutine(HoldNavAgent());
         enemyRb = gameObject.GetComponent<Rigidbody>();
         
