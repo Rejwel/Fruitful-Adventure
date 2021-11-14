d6b4e05f68e7c0c1f31d562bb2754a33d7b700e8
Pawel Dera <paweldera01@gmail.com>
2021-11-14 11:39:33 +0100

    Refactored player prefab

--- Assets/Scripts/FirstPersonCamera/MouseLook.cs
+++ Assets/Scripts/FirstPersonCamera/MouseLook.cs
@@ -3,36 +3,32 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-     public float mouseSensitivity = 100f;
+    public float mouseSensitivity = 100f;
+    [SerializeField] private Transform playerBody;
 
-    public Transform playerBody;
-
-    float xRotation = 0f;
+    private float clampAngle = 80.0f;
+    private float xRotation;
+    private float yRotation;
     private float mouseX;
-
     private float mouseY;
-    // Start is called before the first frame update
     void Awake()
     {
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
-        mouseX = Input.GetAxis("Mouse X")*mouseSensitivity*Time.deltaTime;
-        mouseY = Input.GetAxis("Mouse Y")*mouseSensitivity*Time.deltaTime;
-
-        xRotation -= mouseY;
-        xRotation = Mathf.Clamp(xRotation, -90f, 90f);
-
-        transform.localRotation = Quaternion.Euler(xRotation, 0f , 0f);
-        playerBody.Rotate(Vector3.up * mouseX);
-    }
-
-    public float playerRotation()
-    {
-        return mouseX;
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = -Input.GetAxis("Mouse Y");
+ 
+        yRotation += mouseX * mouseSensitivity * Time.deltaTime;
+        xRotation += mouseY * mouseSensitivity * Time.deltaTime;
+ 
+        xRotation = Mathf.Clamp(xRotation, -clampAngle, clampAngle);
+        
+        Quaternion localRotation = Quaternion.Euler(xRotation, yRotation, 0.0f);
+        transform.rotation = localRotation;
+        playerBody.rotation = Quaternion.Euler(0.0f, yRotation, 0.0f);
     }
 }
