ba13b324c34e23063a7dd3fde7ed03d43622fcda
Mateusz Banasik <kilifugame@gmail.com>
2021-11-23 12:34:18 +0100

    Mapa na klikniecie

--- Assets/Scripts/Minimap/MiniMapScript.cs
+++ Assets/Scripts/Minimap/MiniMapScript.cs
@@ -7,6 +7,7 @@ public class MiniMapScript : MonoBehaviour
 {
     private Vector3 _defaultTransform;
     private Vector3 _defaultScale;
+    private bool _isOpen = false;
 
     private void Start()
     {
@@ -16,15 +17,16 @@ public class MiniMapScript : MonoBehaviour
 
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.M))
+        if (Input.GetKeyDown(KeyCode.M) && !_isOpen)
         {
             transform.localPosition = _defaultTransform + new Vector3(80,-80,0);
             transform.localScale = new Vector3(3.2f, 3.2f, 1);
-        }
-        if (Input.GetKeyUp(KeyCode.M))
+            _isOpen = true;
+        } else if (Input.GetKeyDown(KeyCode.M) && _isOpen)
         {
             transform.localPosition = _defaultTransform;
             transform.localScale = _defaultScale;
+            _isOpen = false;
         }
         
     }
