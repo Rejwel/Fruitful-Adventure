f4f6c6a28eb70e1d48a7c09942d0c478f9744dd2
Pawel Dera <paweldera01@gmail.com>
2021-11-24 17:07:47 +0100

    Refactored player mouse, fixed open shop camera bugs

--- Assets/Scripts/FirstPersonCamera/MouseLook.cs
+++ Assets/Scripts/FirstPersonCamera/MouseLook.cs
@@ -4,19 +4,22 @@ using UnityEngine;
 
 public class MouseLook : MonoBehaviour
 {
-    [SerializeField] private float mouseSensitivity = 1f;
+    [SerializeField] private float mouseSensitivity = 150f;
     
     [SerializeField] private GameObject player;
     [SerializeField] private GameObject camera;
 
     private float _clampAngle = 80.0f;
-    private float _xRot;
-    private float _yRot;
-    private float _xCurrRot;
-    private float _yCurrRot;
-    private float _xRotVelocity;
-    private float _yRotVelocity;
-    private float _smoothDampTime = 0.02f;
+    private float mouseX;
+    private float mouseY;
+    private float xRotation;
+
+    private Quaternion _lookingCameraRotation;
+    private Quaternion _lookingPlayerRotation;
+
+    private bool _isLookingAt = false;
+    private float _lookingTime = 3f;
+    private float _lookingCounter = 0f;
 
     void Awake()
     {
@@ -25,14 +28,43 @@ public class MouseLook : MonoBehaviour
     }
     void Update()
     {
-        _xRot += -Input.GetAxis("Mouse Y") * mouseSensitivity;
-        _yRot += Input.GetAxis("Mouse X") * mouseSensitivity;
-        _xRot = Mathf.Clamp(_xRot,-_clampAngle,_clampAngle);
+        
+        if (_isLookingAt)
+        {
+            if (_lookingCounter >= _lookingTime) CancelLookingAtObject();
+            _lookingTime += Time.time;
+        }
+        else
+        {
+            PlayerMouseMove();
+        }
+    }
 
-        _xCurrRot = Mathf.SmoothDamp(_xCurrRot, _xRot, ref _xRotVelocity, _smoothDampTime);
-        _yCurrRot = Mathf.SmoothDamp(_yCurrRot, _yRot, ref _yRotVelocity, _smoothDampTime);
+    private void PlayerMouseMove()
+    {
+        mouseX = Input.GetAxis("Mouse X") * mouseSensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * mouseSensitivity * Time.deltaTime;
 
-        camera.transform.rotation = Quaternion.Euler(_xCurrRot,_yCurrRot,0f);
-        player.transform.rotation = Quaternion.Euler(0f,_yCurrRot,0f);
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation,-_clampAngle,_clampAngle);
+
+        camera.transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+        player.transform.Rotate(Vector3.up * mouseX * mouseSensitivity/2 * Time.deltaTime);
+        
+    }
+
+    public void LookAtObject(GameObject focusPoint)
+    {
+        xRotation = 0;
+        _lookingCounter += Time.time + _lookingTime;
+        _isLookingAt = true;
+
+        camera.transform.localRotation = Quaternion.Lerp(camera.transform.localRotation, Quaternion.Euler(0,0,0), 6f * Time.deltaTime);
+        player.transform.rotation = Quaternion.Lerp(player.transform.rotation, focusPoint.transform.rotation, 3f * Time.deltaTime);
+    }
+
+    public void CancelLookingAtObject()
+    {
+        _isLookingAt = false;
     }
 }
